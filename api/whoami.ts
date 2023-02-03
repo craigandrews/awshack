b38ab779656f2911310fae94a778dfe6d08b6fb2
Craig Andrews <craig@lazyrobot.org.uk>
2023-02-03 14:48:23 +0000

    tidy up

--- api/whoami.ts
+++ api/whoami.ts
@@ -1,9 +1,12 @@
-import { STSClient, GetCallerIdentityCommand } from "@aws-sdk/client-sts"; // ES Modules import
+import { STSClient, GetCallerIdentityCommand } from "@aws-sdk/client-sts";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   const client = new STSClient({});
-  const command = new GetCallerIdentityCommand({});
-  const response = await client.send(command);
-  return res.json(response.Arn.replace(response.Account, "ACCOUNT_ID"));
+  const response = await client.send(
+    new GetCallerIdentityCommand({}),
+  );
+  return res.json({
+    "whoami": response.Arn.replace(response.Account, "ACCOUNT_ID"),
+  });
 };
